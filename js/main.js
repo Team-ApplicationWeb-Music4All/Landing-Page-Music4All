d49f018d1d908d440a7d73b0d3f11fc11b25ed85
J_Arturo <73813395+pepper419@users.noreply.github.com>
2022-09-29 19:06:24 -0500

    changes

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.nav');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+/* Switch */
+const check = document.querySelector('.check');
+check.addEventListener('click', () => {
+    let id = check.checked;
+    console.log(id);
+    if (id == true) {
+        location.href = 'en/index.html';
+    } else {
+        location.href = 'index.html';
+    }
+    check.checked = id;
+});
+
+/* Slider script */
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+
+let images = [];
+let index = 0;
+
+images[0] = 'assets/images/step-login.png';
+images[1] = 'assets/images/step-search.png';
+images[2] = 'assets/images/step-reservation.png';
+
+btnLeft.addEventListener('click', () => {
+    changeImage(-1);
+})
+
+btnRight.addEventListener('click', () => {
+    changeImage(1);
+})
+
+function changeImage (dir) {
+    document.querySelectorAll('.circle')[index].style = 'color: #C4C4C4';
+    document.querySelectorAll('.step-text')[index].style = 'display: none';
+    index = index + dir;
+
+    if (index < 0) {
+        index = 2;
+    } else if (index > 2) {
+        index = 0;
+    }
+
+    document.querySelector('#slider-image').src = images[index];
+    document.querySelectorAll('.circle')[index].style = 'color: #CB3234';
+    document.querySelectorAll('.step-text')[index].style = 'display: block';
+}
+
